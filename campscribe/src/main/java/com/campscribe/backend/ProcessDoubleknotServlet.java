5650ebe959ec059b1981caec2ba474626d54aae0
jcombs <jcombs10@users.noreply.github.com>
2012-05-06 14:27:08 +0000

    set up doubleknot backend

---
+++ campscribe/src/main/java/com/campscribe/backend/ProcessDoubleknotServlet.java
@@ -0,0 +1,192 @@
+package com.campscribe.backend;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.time.DateUtils;
+
+import au.com.bytecode.opencsv.CSVReader;
+
+import com.campscribe.business.ClazzManager;
+import com.campscribe.business.EventManager;
+import com.campscribe.business.MeritBadgeManager;
+import com.campscribe.business.ScoutManager;
+import com.campscribe.controller.web.UploadController;
+import com.campscribe.model2.Clazz;
+import com.campscribe.model2.Event;
+import com.campscribe.model2.MeritBadge;
+import com.campscribe.model2.Scout;
+import com.googlecode.objectify.Key;
+
+public class ProcessDoubleknotServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 3262454164072964415L;
+
+	private static final Logger log = Logger.getLogger(UploadController.class.getName());
+
+	private ScoutManager scoutManager = new ScoutManager();
+	private EventManager eventManager = new EventManager();
+	private ClazzManager clazzManager = new ClazzManager();
+	private MeritBadgeManager mbManager = new MeritBadgeManager();
+	private String[] datePattern = {"MM/dd/yyyy"};
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+
+		String csvString = req.getParameter("csv");
+
+		CSVReader reader = new CSVReader(new StringReader(csvString));
+		String [] nextLine;
+		Key<Scout> lastScout = null;
+		Key<Event> lastEvent = null;
+		while ((nextLine = reader.readNext()) != null) {
+			log.info("processing line " + Arrays.toString(nextLine));
+			//header or blank line found, skip to the next line
+			if (nextLine.length==0) {
+				log.warning("skipping blank line");
+				continue;
+			} else if ("Group (Registration)".equals(nextLine[0])) {
+				log.info("skipping header line");
+				continue;
+			}
+
+			Key<Scout> s = processScout(nextLine);
+			if (s == null) {
+				log.warning("could not find or create scout " + nextLine[1] + " " + nextLine[2]);
+				continue;
+			} else {
+				Key<Event> e = lastEvent;
+				if (lastScout==null || s.getId()!=lastScout.getId()) {
+					log.info("detected change of scout, checking for new event");
+					e = processEvent(nextLine[3], nextLine[5], nextLine[6]);
+				} else {
+					log.info("same scout as last line, checking for new clazz");
+					if (e == null) {
+						log.warning("could not find or create event " + nextLine[3]);
+						continue;
+					} else {
+						Key<Clazz> c = processClazz(e, nextLine[3]);
+						if (c == null) {
+							log.warning("could not find or create clazz " + nextLine[3]);
+							continue;
+						} else {
+							List<Long> scoutList = new ArrayList<Long>();
+							scoutList.add(s.getId());
+							clazzManager.addScoutsToClazz(c, scoutList);
+							log.info("added scout " + s.getId() + " to clazz " + c.getId());
+						}
+					}
+				}
+
+				lastScout = s;
+				lastEvent = e;
+			}
+
+		}
+
+		log.severe("finished backend processing");
+	}
+
+	private Key<Scout> processScout(String[] nextLine) {
+		String[] unitFields = nextLine[0].split("[ (]");
+		String unitType = unitFields[0];
+		String unitNumber = unitFields[1];
+		String lastName = nextLine[1];
+		String firstName = nextLine[2];
+		Scout aScout = scoutManager.getScout(firstName, lastName, unitType, unitNumber);
+		Key<Scout> sKey = null;
+
+		if (aScout == null) {
+			//create new scout
+			Scout newScout = new Scout(firstName, lastName, "", unitType, unitNumber);
+			sKey = scoutManager.addScout(newScout);
+			log.info("scout created with id " + sKey.getId());
+		} else {
+			log.info("scout found with id " + aScout.getId());
+			sKey = new Key<Scout>(Scout.class, aScout.getId());
+		}
+
+		return sKey;
+	}
+
+	private Key<Event> processEvent(String eventName, String startDateStr, String endDateStr) {
+		Key<Event> returnKey = null;
+		Event anEvent = eventManager.getEvent(eventName);
+
+		if (anEvent == null) {
+			//create new event
+			Date startDate = null;
+			Date endDate = null;
+			try {
+				startDate = DateUtils.parseDateStrictly(startDateStr, datePattern);
+				startDate = DateUtils.addDays(startDate, 1);
+			} catch (ParseException e) {
+				log.log(Level.SEVERE, "start date parse failure", e);
+			}
+			try {
+				endDate = DateUtils.parseDateStrictly(endDateStr, datePattern);
+				endDate = DateUtils.addDays(endDate, -1);
+			} catch (ParseException e) {
+				log.log(Level.SEVERE, "end date parse failure", e);
+			}
+			Event newEvent = new Event(eventName, startDate, endDate);
+			returnKey = eventManager.addEvent(newEvent);
+			log.info("event created with id " + returnKey.getId());
+		} else {
+			log.info("event found with id " + anEvent.getId());
+			returnKey = new Key<Event>(Event.class, anEvent.getId());
+		}
+
+		return returnKey;
+	}
+
+	private Key<Clazz> processClazz(Key<Event> e, String clazzStr) {
+		Key<Clazz> returnKey = null;
+		Clazz aClazz = null;
+		String[] clazzFields = clazzStr.split(" ");
+
+		if (clazzFields.length > 0) {
+			String clazzDescription = clazzFields[clazzFields.length-1];
+			List<String> mbNameParts = new ArrayList<String>();
+			for (int i=0; i<clazzFields.length-1; i++) {
+				mbNameParts.add(clazzFields[i]);
+			}
+			String mbName = StringUtils.join(mbNameParts, " ");
+			log.info("searching for merit badge " + mbName);
+			MeritBadge mb = mbManager.getMeritBadge(mbName);
+
+			if (mb == null) {
+				log.warning("couldn't find merit badge " + mbName);
+			} else {
+				aClazz = eventManager.getClazz(e, clazzDescription, mb.getId());
+				if (aClazz == null) {
+					Clazz newClazz = new Clazz(clazzDescription, mb.getId());
+					returnKey = eventManager.addClazz(e.getId(), newClazz);
+					log.info("clazz created with id " + returnKey.getId());
+				} else {
+					log.info("clazz found with id " + aClazz.getId());
+					returnKey = new Key<Clazz>(e, Clazz.class, aClazz.getId());
+				}
+			}
+		} else {
+			log.warning("couldn't figure out what the clazz description is");
+		}
+
+		return returnKey;
+	}
+
+}
--- campscribe/src/main/java/com/campscribe/controller/web/UploadController.java
+++ campscribe/src/main/java/com/campscribe/controller/web/UploadController.java
@@ -1,48 +1,28 @@
 package com.campscribe.controller.web;
 
+import java.io.DataInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.text.ParseException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import org.apache.commons.lang.StringUtils;
-import org.apache.commons.lang.time.DateUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.multipart.MultipartFile;
 
-import au.com.bytecode.opencsv.CSVReader;
-
-import com.campscribe.business.ClazzManager;
-import com.campscribe.business.EventManager;
-import com.campscribe.business.MeritBadgeManager;
-import com.campscribe.business.ScoutManager;
 import com.campscribe.model.UploadItem;
-import com.campscribe.model2.Clazz;
-import com.campscribe.model2.Event;
-import com.campscribe.model2.MeritBadge;
-import com.campscribe.model2.Scout;
-import com.googlecode.objectify.Key;
+import com.google.appengine.api.backends.BackendServiceFactory;
+import com.google.appengine.api.taskqueue.Queue;
+import com.google.appengine.api.taskqueue.QueueFactory;
+import com.google.appengine.api.taskqueue.TaskOptions;
+import com.google.appengine.api.taskqueue.TaskOptions.Method;
 
 
 @Controller
 @RequestMapping(value = "/upload.cs")
-public class UploadController
-{
+public class UploadController {
 	private static final Logger log = Logger.getLogger(UploadController.class.getName());
-
-	private ScoutManager scoutManager = new ScoutManager();
-	private EventManager eventManager = new EventManager();
-	private ClazzManager clazzManager = new ClazzManager();
-	private MeritBadgeManager mbManager = new MeritBadgeManager();
-	private String[] datePattern = {"MM/dd/yyyy"};
 
 	@RequestMapping(method = RequestMethod.GET)
 	public String getUploadForm(Model model)
@@ -56,144 +36,31 @@ public class UploadController
 
 		MultipartFile fileData = commentForm.getFileData();
 
-		CSVReader reader = new CSVReader(new InputStreamReader(fileData.getInputStream()));
-		String [] nextLine;
-		Key<Scout> lastScout = null;
-		Key<Event> lastEvent = null;
-		while ((nextLine = reader.readNext()) != null) {
-			log.info("processing line " + Arrays.toString(nextLine));
-			//header or blank line found, skip to the next line
-			if (nextLine.length==0) {
-				log.warning("skipping blank line");
-				continue;
-			} else if ("Group (Registration)".equals(nextLine[0])) {
-				log.info("skipping header line");
-				continue;
-			}
-
-			Key<Scout> s = processScout(nextLine);
-			if (s == null) {
-				log.warning("could not find or create scout " + nextLine[1] + " " + nextLine[2]);
-				continue;
-			} else {
-				Key<Event> e = lastEvent;
-				if (lastScout==null || s.getId()!=lastScout.getId()) {
-					log.info("detected change of scout, checking for new event");
-					e = processEvent(nextLine[3], nextLine[5], nextLine[6]);
-				} else {
-					log.info("same scout as last line, checking for new clazz");
-					if (e == null) {
-						log.warning("could not find or create event " + nextLine[3]);
-						continue;
-					} else {
-						Key<Clazz> c = processClazz(e, nextLine[3]);
-						if (c == null) {
-							log.warning("could not find or create clazz " + nextLine[3]);
-							continue;
-						} else {
-							List<Long> scoutList = new ArrayList<Long>();
-							scoutList.add(s.getId());
-							clazzManager.addScoutsToClazz(c, scoutList);
-							log.info("added scout " + s.getId() + " to clazz " + c.getId());
-						}
-					}
-				}
-
-				lastScout = s;
-				lastEvent = e;
-			}
-
+		DataInputStream dis = new DataInputStream(fileData.getInputStream());
+		int formDataLength = (int) fileData.getSize();
+		byte dataBytes[] = new byte[formDataLength];
+		int byteRead = 0;
+		int totalBytesRead = 0;
+		//this loop converting the uploaded file into byte code
+		while (totalBytesRead < formDataLength) {
+		  byteRead = dis.read(dataBytes, totalBytesRead,formDataLength);
+		  totalBytesRead += byteRead;
 		}
-
+		String file = new String(dataBytes);
+		log.log(Level.FINE, "adding to OrderQueue {0}", file);
+		// Create Task and push it into Task Queue
+		createBackendTask(file);
+		
 		return "redirect:/admin.cs"; // redirect after processing!!!
 	}
 
-	private Key<Scout> processScout(String[] nextLine) {
-		String[] unitFields = nextLine[0].split("[ (]");
-		String unitType = unitFields[0];
-		String unitNumber = unitFields[1];
-		String lastName = nextLine[1];
-		String firstName = nextLine[2];
-		Scout aScout = scoutManager.getScout(firstName, lastName, unitType, unitNumber);
-		Key<Scout> sKey = null;
-
-		if (aScout == null) {
-			//create new scout
-			Scout newScout = new Scout(firstName, lastName, "", unitType, unitNumber);
-			sKey = scoutManager.addScout(newScout);
-			log.info("scout created with id " + sKey.getId());
-		} else {
-			log.info("scout found with id " + aScout.getId());
-			sKey = new Key<Scout>(Scout.class, aScout.getId());
-		}
-
-		return sKey;
+	private void createBackendTask(String data) {
+		 // Create Task and push it into Task Queue
+		Queue queue = QueueFactory.getQueue("UploadQueue");
+		TaskOptions taskOptions = TaskOptions.Builder.withUrl("/processDoubleknot")
+		                          .param("csv", data)
+		                          .header("Host", BackendServiceFactory.getBackendService().getBackendAddress("doubleknot-backend"))
+		                          .method(Method.POST);
+		queue.add(taskOptions);	
 	}
-
-	private Key<Event> processEvent(String eventName, String startDateStr, String endDateStr) {
-		Key<Event> returnKey = null;
-		Event anEvent = eventManager.getEvent(eventName);
-
-		if (anEvent == null) {
-			//create new event
-			Date startDate = null;
-			Date endDate = null;
-			try {
-				startDate = DateUtils.parseDateStrictly(startDateStr, datePattern);
-				startDate = DateUtils.addDays(startDate, 1);
-			} catch (ParseException e) {
-				log.log(Level.SEVERE, "start date parse failure", e);
-			}
-			try {
-				endDate = DateUtils.parseDateStrictly(endDateStr, datePattern);
-				endDate = DateUtils.addDays(endDate, -1);
-			} catch (ParseException e) {
-				log.log(Level.SEVERE, "end date parse failure", e);
-			}
-			Event newEvent = new Event(eventName, startDate, endDate);
-			returnKey = eventManager.addEvent(newEvent);
-			log.info("event created with id " + returnKey.getId());
-		} else {
-			log.info("event found with id " + anEvent.getId());
-			returnKey = new Key<Event>(Event.class, anEvent.getId());
-		}
-
-		return returnKey;
-	}
-
-	private Key<Clazz> processClazz(Key<Event> e, String clazzStr) {
-		Key<Clazz> returnKey = null;
-		Clazz aClazz = null;
-		String[] clazzFields = clazzStr.split(" ");
-
-		if (clazzFields.length > 0) {
-			String clazzDescription = clazzFields[clazzFields.length-1];
-			List<String> mbNameParts = new ArrayList<String>();
-			for (int i=0; i<clazzFields.length-1; i++) {
-				mbNameParts.add(clazzFields[i]);
-			}
-			String mbName = StringUtils.join(mbNameParts, " ");
-			log.info("searching for merit badge " + mbName);
-			MeritBadge mb = mbManager.getMeritBadge(mbName);
-
-			if (mb == null) {
-				log.warning("couldn't find merit badge " + mbName);
-			} else {
-				aClazz = eventManager.getClazz(e, clazzDescription, mb.getId());
-				if (aClazz == null) {
-					Clazz newClazz = new Clazz(clazzDescription, mb.getId());
-					returnKey = eventManager.addClazz(e.getId(), newClazz);
-					log.info("clazz created with id " + returnKey.getId());
-				} else {
-					log.info("clazz found with id " + aClazz.getId());
-					returnKey = new Key<Clazz>(e, Clazz.class, aClazz.getId());
-				}
-			}
-		} else {
-			log.warning("couldn't figure out what the clazz description is");
-		}
-
-		return returnKey;
-	}
-
 }
