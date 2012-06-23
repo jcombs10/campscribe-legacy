e44e32b43bcab3a4b834482903fe6d16f7dce94a
jcombs <jcombs@6d6fe5a1-a02e-4d48-9587-3e4c5082c35f>
2012-06-23 20:01:32 +0000

    remove scout from clazz functionality

--- campscribe/src/main/java/com/campscribe/client/ScoutClazzModule.java
+++ campscribe/src/main/java/com/campscribe/client/ScoutClazzModule.java
@@ -1,9 +1,12 @@
 package com.campscribe.client;
 
 import com.campscribe.client.clazzes.AddEditScoutClazzView;
+import com.campscribe.client.clazzes.ClazzService;
+import com.campscribe.client.clazzes.ClazzServiceJSONImpl;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.RootPanel;
 
@@ -11,11 +14,15 @@ import com.google.gwt.user.client.ui.RootPanel;
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class ScoutClazzModule implements EntryPoint {
+
+	ClazzService clazzService = new ClazzServiceJSONImpl();
+
 	/**
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
-		
+		addGWTActionTriggers(this);
+
 		final Button addButton = new Button("Add Scout");
 
 		// We can add style names to widgets
@@ -33,11 +40,26 @@ public class ScoutClazzModule implements EntryPoint {
 
 		// Add a handler to send the name to the server
 		addButton.addClickHandler(new ClickHandler() {
-			
+
 			@Override
 			public void onClick(ClickEvent event) {
 				dialogBox.center();
 			}
 		});
 	}
+
+	private native void addGWTActionTriggers(ScoutClazzModule module)/*-{
+    $wnd.ScoutClazzGWT = {
+        deleteScoutFromClazz: function(eventId, clazzId, scoutId, clazzName, scoutName) {
+            module.@com.campscribe.client.ScoutClazzModule::deleteScoutFromClazz(Ljava/lang/Long;Ljava/lang/Long;Ljava/lang/Long;Ljava/lang/String;Ljava/lang/String;)(eventId, clazzId, scoutId, clazzName, scoutName);
+        }
+    };
+	}-*/;
+
+	public void deleteScoutFromClazz(Long eventId, Long clazzId, Long scoutId, String clazzName, String scoutName) {
+		if (Window.confirm("Are you sure you want to remove "+scoutName+" from "+clazzName+"?")) {
+			clazzService.deleteScoutFromClazz(eventId, clazzId, scoutId);
+		}
+	}
+
 }
